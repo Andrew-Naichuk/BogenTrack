434b2d0b6663dc9d84bf2795396309557349498f
Andrew-Naichuk <andrew.naichuk@gmail.com>
2023-07-07 16:09:35 +0200

    Reports functionality added

--- app.js
+++ app.js
@@ -212,6 +212,104 @@ backHomeButton.addEventListener('click', function(){
 });
 
 
+// Send Report Functionality
+reportIssueButton.addEventListener('click', function(){
+    showScreen('reportScreen');
+    getReports();
+});
+cancelReportButton.addEventListener('click', function(){
+    reportTextField.value = '';
+    showScreen('profileScreen');
+});
+submitReportButton.addEventListener('click', function(){
+    if (reportTextField.value.length > 0) {
+        // Creating model report object
+        // All new reports have 'new' status by default to be tracked as solved or not
+        // This status is used later to solve reports and send notifications to users
+        const newReport = {
+            "uid": generateUID(),
+            "status": 'new',
+            "content": reportTextField.value.replace(/</g, '(').replace(/>/g, ')'),
+            "reply": '',
+        };
+        // Asynchronous function for database operations
+        (async function(){
+            // Get reference to the user document in the btIssues collection
+            const docRef = db.collection("btIssues").doc(auth.currentUser.uid);
+            // Retrieve the document from the database
+            const doc = await docRef.get();
+            // Check if the document exists
+            if (doc.exists) {
+                try {
+                    // Pushing populated report object to the DB by updating the reports array field
+                    await db.collection("btIssues").doc(auth.currentUser.uid).update({
+                        reports: firebase.firestore.FieldValue.arrayUnion(newReport),
+                    });
+                } catch (error) {
+                    window.alert(error);
+                }
+            } else {
+                try {
+                    // Create a new document with reports array field containing the new report object
+                    await db.collection("btIssues").doc(auth.currentUser.uid).set({
+                        reports: firebase.firestore.FieldValue.arrayUnion(newReport),
+                    });
+                } catch (error) {
+                    window.alert(error);
+                }
+            }
+        })();
+        // Setting field value back to default
+        reportTextField.value = '';
+        setTimeout(getReports, 1000);
+    }
+});
+
+async function getReports(){
+    reportsList.innerHTML = '';
+    const docRef = db.collection("btIssues").doc(auth.currentUser.uid);
+    // Retrieve the document from the database
+    try {
+        const doc = await docRef.get();
+        // Check if the document exists
+        if (doc.exists) {
+            let reportsSnapshot = doc.data().reports;
+            // Rendering reports submited by user
+            reportsSnapshot.forEach(report => {
+                const renderedReport = document.createElement("article");
+                if (report.reply && report.reply.length > 0) {
+                    renderedReport.innerHTML = `
+                    <h5>Report ${report.uid}</h5>
+                    <p>${report.content}</p>
+                    <h5>BogenTrack team reply:</h5>
+                    <p>${report.reply}</p>
+                    `
+                } else {
+                    renderedReport.innerHTML = `
+                    <h5>Report ${report.uid}</h5>
+                    <p>${report.content}</p>
+                    <p class="faded"> No reply from BogenTrack team yet.</p>
+                    `
+                };
+                reportsList.appendChild(renderedReport);
+            });
+        } else {
+            // Rendering notice that no reports were submited by user yet
+            const reportsNotice = document.createElement("div");
+            reportsNotice.className = 'screenNotice';
+            reportsNotice.innerHTML = `
+                <div class="faded">${iconsBundle.box}</div>
+                <h4>No Reports</h4>
+                <p>You can help us improve BogenTrack by submiting your issue report or wish.</p>
+            `
+            reportsList.appendChild(reportsNotice);
+        }
+    } catch (error) {
+        window.alert(error)
+    }
+};
+
+
 
 // Add Session - Cancel Add Switching
 createSessionButton.addEventListener('click', function(){
--- elements.js
+++ elements.js
@@ -13,7 +13,7 @@ const iconsBundle = {
     repeat: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="#ABAEB1" d="m19 8l-4 4h3a6 6 0 0 1-6 6c-1 0-1.97-.25-2.8-.7l-1.46 1.46A7.93 7.93 0 0 0 12 20a8 8 0 0 0 8-8h3M6 12a6 6 0 0 1 6-6c1 0 1.97.25 2.8.7l1.46-1.46A7.93 7.93 0 0 0 12 4a8 8 0 0 0-8 8H1l4 4l4-4"/></svg>',
     user: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="#abaeb1" d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2M7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.5.88 4.93 1.78A7.893 7.893 0 0 1 12 20c-1.86 0-3.57-.64-4.93-1.72m11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33A7.928 7.928 0 0 1 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.5-1.64 4.83M12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6m0 5a1.5 1.5 0 0 1-1.5-1.5A1.5 1.5 0 0 1 12 8a1.5 1.5 0 0 1 1.5 1.5A1.5 1.5 0 0 1 12 11Z"/></svg>',
     google: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#abaeb1" d="M21.35 11.1h-9.17v2.73h6.51c-.33 3.81-3.5 5.44-6.5 5.44C8.36 19.27 5 16.25 5 12c0-4.1 3.2-7.27 7.2-7.27c3.09 0 4.9 1.97 4.9 1.97L19 4.72S16.56 2 12.1 2C6.42 2 2.03 6.8 2.03 12c0 5.05 4.13 10 10.22 10c5.35 0 9.25-3.67 9.25-9.09c0-1.15-.15-1.81-.15-1.81Z"/></svg>',
-    box: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#abaeb1" d="M2 10.96a.985.985 0 0 1-.37-1.37L3.13 7c.11-.2.28-.34.47-.42l7.83-4.4c.16-.12.36-.18.57-.18c.21 0 .41.06.57.18l7.9 4.44c.19.1.35.26.44.46l1.45 2.52c.28.48.11 1.09-.36 1.36l-1 .58v4.96c0 .38-.21.71-.53.88l-7.9 4.44c-.16.12-.36.18-.57.18c-.21 0-.41-.06-.57-.18l-7.9-4.44A.991.991 0 0 1 3 16.5v-5.54c-.3.17-.68.18-1 0m10-6.81v6.7l5.96-3.35L12 4.15M5 15.91l6 3.38v-6.71L5 9.21v6.7m14 0v-3.22l-5 2.9c-.33.18-.7.17-1 .01v3.69l6-3.38m-5.15-2.55l6.28-3.63l-.58-1.01l-6.28 3.63l.58 1.01Z"/></svg>',
+    box: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#abaeb1" d="M2 10.96a.985.985 0 0 1-.37-1.37L3.13 7c.11-.2.28-.34.47-.42l7.83-4.4c.16-.12.36-.18.57-.18c.21 0 .41.06.57.18l7.9 4.44c.19.1.35.26.44.46l1.45 2.52c.28.48.11 1.09-.36 1.36l-1 .58v4.96c0 .38-.21.71-.53.88l-7.9 4.44c-.16.12-.36.18-.57.18c-.21 0-.41-.06-.57-.18l-7.9-4.44A.991.991 0 0 1 3 16.5v-5.54c-.3.17-.68.18-1 0m10-6.81v6.7l5.96-3.35L12 4.15M5 15.91l6 3.38v-6.71L5 9.21v6.7m14 0v-3.22l-5 2.9c-.33.18-.7.17-1 .01v3.69l6-3.38m-5.15-2.55l6.28-3.63l-.58-1.01l-6.28 3.63l.58 1.01Z"/></svg>',
     bow: '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path fill="#abaeb1" d="M19.03 6.03L20 7l2-5l-5 2l.97.97l-1.82 1.82C10.87 2.16 3.3 3.94 2.97 4L2 4.26l.5 1.94l.79-.2l6.83 6.82L6.94 16H5l-3 3l2 1l1 2l3-3v-1.94l3.18-3.18L18 20.71l-.19.79l1.93.5l.26-.97c.06-.33 1.84-7.9-2.79-13.18l1.82-1.82M4.5 5.78c2.05-.28 6.78-.5 10.23 2.43l-3.91 3.91L4.5 5.78M18.22 19.5l-6.34-6.32l3.91-3.91c2.93 3.45 2.71 8.18 2.43 10.23Z"/></svg>',
 
 }
@@ -108,3 +108,12 @@ const backHomeButton = document.querySelector('#backHomeBtn');
 const signOutButton = document.querySelector('#signOutBtn');
 const userEmailIndicator = document.querySelector('#currentUserEmail');
 const userSessionsIndicator = document.querySelector('#currentNumberOfSessions');
+const reportIssueButton = document.querySelector('#reportIssueBtn');
+
+
+// Report Screen
+const reportScreen = document.querySelector('#reportScreen');
+const reportsList = document.querySelector('#reportsList');
+const reportTextField = document.querySelector('#reportText');
+const submitReportButton = document.querySelector('#submitReportBtn');
+const cancelReportButton = document.querySelector('#cancelReportBtn');
